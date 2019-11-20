28067ce15f1b6cef0ad2ed848dbdcacb240a33e1
Sana Zaidi <sana.rizvi.5@gmail.com>
2019-11-20 21:29:46 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def line(katz_deli)
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else 
+    current_line = 'The line is currently: '
+    katz_deli.each_with_index do |person, index|
+      current_line << "#{index + 1}. #{person} "
+    end
+    
+    puts current_line
+  end
+end
